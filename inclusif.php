16864bd7788ce5eae9079e159c1f487db1e2b803
Bunker D <contact@bunkerd.fr>
2018-03-18 23:31:01 -0500

    Options implémentées. Plugin fonctionnel.

--- inclusif.php
+++ inclusif.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Inclusif
- * Version:     0.1
+ * Version:     1.0
  * Plugin URI:  -  TODO  -
  * Author:      Bunker D
  * Author URI:  http://bunkerd.fr/
@@ -14,77 +14,14 @@
  *
  * @package Inclusif
  * @author  Bunker D
- * @version 0.1
+ * @version 1.0
  */
 
 defined( 'ABSPATH' ) or die();
 
-
-/* https://codex.wordpress.org/Writing_a_Plugin
-
-Use Filters:
-
-    For instance, before WordPress adds the title of a post to browser output, it first checks to see if any Plugin has registered a function for the "filter" hook called "the_title".
-
-Options:
-
-    See: https://codex.wordpress.org/Creating_Options_Pages
-    It's also generally considered a good idea to minimize the number of options you use for your plugin. For example, instead of storing 10 different named options consider storing a serialized array of 10 elements as a single named option.
-
-    Create or update value:    update_option($option_name, $newvalue);
-    Get value.                 get_option($option);
-
-
-https://codex.wordpress.org/Determining_Plugin_and_Content_Directories
-*/
-
-/* https://codex.wordpress.org/Plugin_API
-
-Filter Functions
-    has_filter()
-    add_filter()
-    apply_filters()
-    apply_filters_ref_array()
-    current_filter()
-    remove_filter()
-    remove_all_filters()
-    doing_filter()
-
-Actions Functions
-    has_action()
-    add_action()
-    do_action()
-    do_action_ref_array()
-    did_action()
-    remove_action()
-    remove_all_actions()
-    doing_action()
-
-Activation/Deactivation/Uninstall Functions
-    register_activation_hook()
-    register_deactivation_hook()
-    register_uninstall_hook()
-
-Relevant filters:
-    the_content
-    the_title
-    comment_text
-    the_excerpt
-    get_comment_text
-    get_comment_excerpt
-    (https://codex.wordpress.org/Plugin_API/Filter_Reference)
-
-*/
-
 function bd_incl_replace( $content ) {
-//    $options         = $this->get_options();
-//    $text_to_replace = apply_filters( 'c2c_text_replace',                $options['text_to_replace'] );
-//    $case_sensitive  = apply_filters( 'c2c_text_replace_case_sensitive', $options['case_sensitive'] );
-//    $limit           = apply_filters( 'c2c_text_replace_once',           $options['replace_once'] ) ? 1 : -1;
-//    $preg_flags      = $case_sensitive ? 's' : 'si';
-//    $mb_regex_encoding = null;
 
-    $sep = '·';
+    $sep = get_option('bd_inclusif_sep');
     $content = ' ' . $content . ' ';
 
     // Set the encoding to UTF-8.
@@ -95,6 +32,7 @@ function bd_incl_replace( $content ) {
         $encoding = null;
     }
 
+    // Do the relevant substitutions
     $srch = array(
                 '~(?!<.*?)(?<=(?!<\pL[^>]*?)[\s>])((?:\pP|&\pL*;|&#\d*;|"|\'|“|‘|«|…)*?)(?!http|www)(\pL+)(?:\.|·|•)(?!com|gov|edu|org|xyz|int|fr|be|ch|de|uk|ca|es|it|quebec)(\pL+)(?=(\pP|&\pL*;|&#\d*;|"|\'|”|’|»|…)*?[\s<])(?![^<>]*?>)~Ss',
                 '~(?!<.*?)(?<=[\s>])((?:\pP|&\pL*;|&#\d*;|"|\'|“|‘|«|…)*?)(?!http|www)(\pL+)(?:\.|·|•)(\pL+)(?:\.|·|•)s(?=(\pP|&\pL*;|&#\d*;|"|\'|”|’|»|…)*?[\s<])(?![^<>]*?>)~Ss'
@@ -112,6 +50,55 @@ function bd_incl_replace( $content ) {
 
     return trim( $content );
 }
-add_filter( 'comment_text' , 'bd_incl_replace' );
 add_filter( 'the_title'    , 'bd_incl_replace' );
 add_filter( 'the_content'  , 'bd_incl_replace' );
+if ( get_option('bd_inclusif_comm') ) {
+    add_filter( 'comment_text' , 'bd_incl_replace' );
+}
+
+
+add_action('admin_menu', function() {
+    add_options_page( 'Configuration du texte inclusif', 'Texte inclusif', 'manage_options', 'inclusif', 'bd_inclusif_options_page' );
+});
+
+add_action( 'admin_init', function() {
+    register_setting( 'bd-inclusif-options', 'bd_inclusif_sep' );
+    register_setting( 'bd-inclusif-options', 'bd_inclusif_comm' );
+});
+
+function bd_inclusif_options_page() {
+?>
+    <div class="wrap">
+        <form action="options.php" method="post">
+            <?php
+                settings_fields( 'bd-inclusif-options' );
+                do_settings_sections( 'bd-inclusif-options' );
+            ?>
+            <h1>Configuration du texte inclusif</h1>
+            <table class="form-table">
+                <tbody>
+                    <tr>
+                        <th>Séprataur utilisé :</th>
+                        <td>
+                            <select name="bd_inclusif_sep">
+                            <option value="·" <?php echo esc_attr( get_option('bd_inclusif_sep') ) == '·' ? 'selected="selected"' : ''; ?>>Point médian : ·</option>
+                            <option value="." <?php echo esc_attr( get_option('bd_inclusif_sep') ) == '.' ? 'selected="selected"' : ''; ?>>Point bas : .</option>
+                            <option value="•" <?php echo esc_attr( get_option('bd_inclusif_sep') ) == '•' ? 'selected="selected"' : ''; ?>>Point gras : •</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Traitement des commentaires :</th>
+                        <td>
+                            <input type="checkbox" name="bd_inclusif_comm" <?php echo esc_attr( get_option('bd_inclusif_comm') ) == 'on' ? 'checked="checked"' : ''; ?> />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td><?php submit_button(); ?></td>
+                    </tr>
+                </tbody>
+            </table>
+        </form>
+    </div>
+<?php
+}
